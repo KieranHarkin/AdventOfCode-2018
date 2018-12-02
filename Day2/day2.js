cd29b549fbca9eda66053f64a49f8f25feaee0a1
KieranHarkin <kieranharkin@googlemail.com>
2018-12-02 23:17:21 +0000

    tidy up

--- Day2/day2.js
+++ Day2/day2.js
@@ -5,35 +5,30 @@ const getInput = () => {
 }
 
 const part1 = () => {
+    let twoCount = 0;
+    let threeCount = 0;
 
-let twoCount = 0;
-let threeCount = 0;
+    const input = getInput();
 
-const input = getInput();
+    for(const val of input) {
+        let chars = val.split('').sort();
+        let hasTwo = false;
+        let hasThree = false;
 
-for(const val of input) {
-    let chars = val.split('').sort();
-    let hasTwo = false;
-    let hasThree = false;
+        for(const char of chars) {
+            const x = chars.lastIndexOf(char);
 
-    for(const char of chars) {
-        const x = chars.lastIndexOf(char);
-
-        if (x === 1 & !hasTwo) {
-            twoCount++;
-            hasTwo = true;
-        } else if (x === 2 &!hasThree) {
-            threeCount++;
-            hasThree = true;
-        }    
-        chars = chars.slice(x+1, chars.length);
+            if (x === 1 & !hasTwo) {
+                twoCount++;
+                hasTwo = true;
+            } else if (x === 2 &!hasThree) {
+                threeCount++;
+                hasThree = true;
+            }    
+            chars = chars.slice(x+1, chars.length);
+        }
     }
-}
-return twoCount * threeCount;
+    return twoCount * threeCount;
 }
 
 console.log(part1());
-
-
-
-
